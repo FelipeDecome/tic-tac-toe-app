96a901efabda0ed5c830545b016cd8fb458e1227
Felipe Decome <decome.felipe@gmail.com>
2023-09-04 10:34:39 -0300

    feat: tests for create room service

---
+++ api/test/unit/modules/matches/service/createRoomService.spec.ts
@@ -0,0 +1,36 @@
+import { CreateRoomService } from '@modules/matches/service/createRoomService';
+import { IRoomRepository } from '@modules/matches/repository/interfaces/IRoomRepository';
+import { Test } from '@nestjs/testing';
+
+describe('CreateRoomService', () => {
+  let service: CreateRoomService;
+  let repository: IRoomRepository;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        CreateRoomService,
+        {
+          provide: IRoomRepository,
+          useValue: {
+            create: jest.fn().mockImplementation(() => ({
+              id: '1',
+              code: '123456',
+            })),
+          },
+        },
+      ],
+    }).compile();
+
+    service = moduleRef.get(CreateRoomService);
+    repository = moduleRef.get(IRoomRepository);
+  });
+
+  it('should create a room', async () => {
+    const create = jest.spyOn(repository, 'create');
+
+    await service.execute();
+
+    expect(create).toBeCalledTimes(1);
+  });
+});
