dfff8ccc6c5b9ca2ff1bfdeb1a23a552f4c69f8f
Felipe Decome <decome.felipe@gmail.com>
2023-09-04 09:21:15 -0300

    chore: updating database

--- api/prisma/migrations/20230904114645_create_room/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Room" (
-    "id" TEXT NOT NULL,
-    "code" VARCHAR(6) NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Room_pkey" PRIMARY KEY ("id")
-);
