147c96cd99e6e26c76990aa0ab50a9e84927ec8c
Felipe Decome <decome.felipe@gmail.com>
2023-10-27 21:10:47 -0300

    refactor: renaming files to match file naming convention

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@shared/config/config.module';
-import { RoomsModule } from '@modules/rooms/module';
+import { RoomsModule } from '@modules/rooms/rooms.module';
 
 @Module({
   imports: [ConfigModule, RoomsModule],
